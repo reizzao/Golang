237e61e570a0462092fb5bcf1c3a59a216b00fa3
@ReinaldoReizao <60945992+reinaldoreizao@users.noreply.github.com>
2024-06-02 13:13:27 -0300

    feat: polo unico das libs importadas

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/rzjprogramador/base_golang
 
 go 1.20
+
+require github.com/rzjprogramador/Libs_Rzj v0.0.0-20240602155621-cc0d60375027
--- internal/helper/helper.go
+++ internal/helper/helper.go
@@ -1,12 +1,10 @@
 package helper
 
 import (
-	"github.com/rzjprogramador/Libs_Rzj/blob/main/Go_Libs/soma"
-	// "/home/rzj/x/_github_rz_/_NO_GITHUB_/SuperLibs_TS/Go_Libs/soma/soma"
-	// "github.com/rzjprogramador/base_golang/internal/helper/soma"
+	"github.com/rzjprogramador/Libs_Rzj/Go_Libs/soma"
 )
 
-// POLO_UNICO : HELPER : repassar aqui em funcoes publicas todos os Helpers.
+// POLO_UNICO : REPASSA AS LIBS IMPORTADAS
 
 func UseSoma(x int, y int) (int, error) {
 	return soma.Soma(x, y)
