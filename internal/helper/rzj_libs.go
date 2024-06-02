94d7fffb9c5f1f2ec499cd92ba3a82bc2fea33cd
@ReinaldoReizao <60945992+reinaldoreizao@users.noreply.github.com>
2024-06-02 13:14:17 -0300

    ok

--- internal/helper/rzj_libs.go
+++ internal/helper/rzj_libs.go
@@ -4,7 +4,7 @@ import (
 	"github.com/rzjprogramador/Libs_Rzj/Go_Libs/soma"
 )
 
-// POLO_UNICO : REPASSA AS LIBS IMPORTADAS
+// POLO_UNICO : REPASSA AS LIBS IMPORTADAS : Rzj_Libs
 
 func UseSoma(x int, y int) (int, error) {
 	return soma.Soma(x, y)
