9a73cfdee0f77213a27b6a23d445602202e17d5b
@ReinaldoReizao <60945992+reinaldoreizao@users.noreply.github.com>
2024-06-02 13:09:55 -0300

    ok

--- internal/helper/helper.go
+++ internal/helper/helper.go
@@ -1,7 +1,9 @@
 package helper
 
 import (
-	"github.com/rzjprogramador/base_golang/internal/helper/soma"
+	"github.com/rzjprogramador/Libs_Rzj/blob/main/Go_Libs/soma"
+	// "/home/rzj/x/_github_rz_/_NO_GITHUB_/SuperLibs_TS/Go_Libs/soma/soma"
+	// "github.com/rzjprogramador/base_golang/internal/helper/soma"
 )
 
 // POLO_UNICO : HELPER : repassar aqui em funcoes publicas todos os Helpers.
--- internal/helper/soma/seed.go
+++
@@ -1,5 +0,0 @@
-package soma
-
-func HardTester_Soma() (int, error) {
-	return Soma(1, 2)
-}
