617644a499b836b1b18dc41c55d215b0003c284a
@ReinaldoReizao <60945992+reinaldoreizao@users.noreply.github.com>
2024-08-21 11:26:13 -0300

    feat: test go

---
+++ ConceitosGO/garantia_test/base_test.go
@@ -0,0 +1,25 @@
+package garantia_test
+
+import (
+	"testing"
+)
+
+var resultado_certo = 1000
+
+func funcaoalvo() int {
+	return resultado_certo
+}
+
+func TestPessoa(t *testing.T) {
+	sut := funcaoalvo()
+	comparando_com_resultado_certo := 1000
+
+	if sut != comparando_com_resultado_certo {
+		t.Error("Ops... Esperado: ", sut, "Tentativa: ", comparando_com_resultado_certo)
+	}
+
+	// Mais tests
+	// if testado != 9 {
+	// 	t.Error("Ops...")
+	// }
+}
